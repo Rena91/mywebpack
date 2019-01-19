4b14ba8371f20efacde4856be3cc005fea080763
ReNa <Revthati@gmail.com>
2019-01-19 10:11:01 +0530

    webpack1

---
+++ src/about.component.js
@@ -0,0 +1,8 @@
+// contact.component.js
+import React from 'react';
+const AboutComponent = ( props ) => {
+    return (
+        <h1>About Component!</h1>
+    );
+}
+export default AboutComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,60 @@
+'use strict'
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-    entry:{},
-    output:{},
-    loader:{},
-    plugin:{},
-    module:{}
+    mode: "development",
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/react', '@babel/env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors: false,
+                default: false,
+                vendor: {
+                    name:'vendor',
+                    // sync + async chunks
+                    chunks: 'all',
+                    // import file path containing node_modules
+                    test: /node_modules/
+                }
+            }
+        }
+    }
 }
+
+// loader:{},
+//     plugin:{},
